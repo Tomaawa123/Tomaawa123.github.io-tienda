e3472aba54bd85a3deaba66330535a4f9ed9bff0
Tomaawa123 <92940436+Tomaawa123@users.noreply.github.com>
2023-01-22 17:59:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -62,3 +62,53 @@ boton.addEventListener('click', sonar)
 function sonar() {
     audio.play()
 }
+
+//////////////////////////
+document.getElementById("audiobackground").volume = 0.5
+    ////////////////////////////
+var x = document.getElementById("myAudio");
+
+function playAudio() {
+    x.play();
+}
+
+function pauseAudio() {
+    x.pause();
+}
+////////////////////////
+function mostrar() {
+    document.getElementById('premio').style.display = 'block';
+    document.getElementById('triste').style.display = 'none';
+}
+
+function ocultar() {
+    document.getElementById('premio').style.display = 'none';
+    document.getElementById('triste').style.display = 'block';
+}
+//////////////////////////////
+let button = document.querySelector(".reproductor")
+
+button.addEventListener("click", () => {
+        let etiquetaAudio = document.createElement("audio")
+        etiquetaAudio.setAttribute("src", "./sounds/agregar1.wav")
+        etiquetaAudio.play()
+    })
+    /////////////////////
+function mostrar() {
+    div = document.getElementById('flotante');
+    div.style.display = '';
+}
+
+function cerrar() {
+    div = document.getElementById('flotante');
+    div.style.display = 'none';
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', () => {
+    let etiquetaAudio = document.createElement("audio")
+    etiquetaAudio.setAttribute("src", "./sounds/agregar1.wav")
+    etiquetaAudio.play()
+
+
+}));
